9efd8fdeb7dba7ab5b0b8b8768aa8e2d53a15375
d-ball <dball@ncsu.edu>
2014-04-24 22:41:52 -0400

    initial website commit
    what we have so far

---
+++ capture2.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>PHP Test</title>
+</head>
+<body>
+<?php echo '<p>Hello World</p>'; ?>
+<?php
+// Create connection
+$con=mysqli_connect("abilitex.com","csc342","bL1n","csc342");
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?> 
+</body>
+</html>
